c4be873848b240d486adaf8b17913c24b636388c
Jakub Beran <nomail>
2021-01-21 13:24:29 +0100

    day01: Formatting applied

--- day01_arr.py
+++ day01_arr.py
@@ -8,18 +8,18 @@ for x in f:
     # Convert to int and append
     list_of_sums.append(int(x))
 
-# Sort    
+# Sort
 list_of_sums.sort()
 
 # Start of count
 end = len(list_of_sums) - 1
 for i in range(len(list_of_sums)):
-    while list_of_sums[i] + list_of_sums[end] > 2020: 
+    while list_of_sums[i] + list_of_sums[end] > 2020:
         end -= 1
-        
+
     if list_of_sums[i] + list_of_sums[end] == 2020:
         print("{} + {} = 2020".format(str(list_of_sums[i]), str(list_of_sums[end])))
-    
+
     # Check if the list was exhausted
     if i + 1 >= end:
         break
--- day01_part2.py
+++ day01_part2.py
@@ -7,17 +7,20 @@ f = open(data_path, "r")
 for x in f:
     # Convert to int
     x = int(x)
-    
+
     # Go over the list of inputs - brute force approach
     if len(list_of_sums) > 0:
         for r in range(len(list_of_sums)):
             for l in range(r + 1, len(list_of_sums)):
                 if list_of_sums[r] + list_of_sums[l] + x == 2020:
-                    print("{}*{}*{} = {}".format(
-                            str(x), 
-                          str(list_of_sums[r]), 
-                          str(list_of_sums[l]), 
-                          str(list_of_sums[r]*list_of_sums[l]*x)))
-        
+                    print(
+                        "{}*{}*{} = {}".format(
+                            str(x),
+                            str(list_of_sums[r]),
+                            str(list_of_sums[l]),
+                            str(list_of_sums[r] * list_of_sums[l] * x),
+                        )
+                    )
+
     # Add the input to the list
     list_of_sums.append(x)
